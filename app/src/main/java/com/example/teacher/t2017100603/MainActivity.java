3aa9a0b89848cf2a09d3bf142f5ac1a72264bdeb
Shaojung Lu <shaojung@gmail.com>
2017-10-06 14:00:54 +0800

    get sharedpreferences.

--- app/src/main/java/com/example/teacher/t2017100603/MainActivity.java
+++ app/src/main/java/com/example/teacher/t2017100603/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.teacher.t2017100603;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent it = new Intent(MainActivity.this, SettingsActivity.class);
         startActivity(it);
+    }
+    public void clickRead(View v)
+    {
+        SharedPreferences sp = getSharedPreferences(getPackageName() + "_preferences", MODE_PRIVATE);
+        String str = sp.getString("example_text", "");
+        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
     }
 }
